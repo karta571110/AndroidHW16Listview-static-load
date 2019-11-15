d472b2e0cf685fc0102412b8e7e78713740bc1d0
karta571110 <karta571110@gmail.com>
2019-11-16 03:19:30 +0800

    finish

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView=(TextView)findViewById(R.id.textView);
+        ListView staticlistview=(ListView)findViewById(R.id.listview);
+        staticlistview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                textView.setText("你選擇了:"+((TextView)view).getText());
+            }
+        });
+    }
+}
